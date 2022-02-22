b03a5bd1c6a9cbe835d925af39c6b1f7634c219a
Superscoth <pierre.delsirie@epitech.eu>
2022-02-22 15:13:29 +0100

    [ADD] Standart Deviation for Histograms

--- analyze.py
+++ analyze.py
@@ -2,11 +2,13 @@ import pandas as pd
 
 dtypes = {"Car": "str", "MPG": "float", "Cylinders": "int", "Displacement": "float", "Horsepower": "float", "Weight": "float", "Acceleration": "float", "Model": "int", "Origin": "str"}
 
-df = pd.read_csv("./cars.csv", header=0, dtype=dtypes)
+df = pd.DataFrame(pd.read_csv("./cars.csv", header=0, dtype=dtypes))
 
 #Generate Histograms for MPG
 print('---\nGenerate Histogram for MPG')
 df.hist(column='MPG')
+#Example to print only one column
+#print(df.std()['MPG'])
 
 print('---\nGenerate Histogram for Cylindres')
 df.hist(column='Cylinders')
@@ -25,3 +27,7 @@ df.hist(column='Acceleration')
 
 print('---\nGenerate Histogram for Model')
 df.hist(column='Model')
+
+#Print the standart Deviation of every Histograms
+print('---\nGenerate Standart Deviation')
+print(df.std())
