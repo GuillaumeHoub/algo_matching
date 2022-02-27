aae34c5a53bb20e2decd1929bd5f1ede01c9058d
GuillaumeHoub <guilllaume.houbion@epitech.eu>
2022-02-27 11:38:52 +0100

    [FIX] number_of_clusters and cluster.py

--- cluster.py
+++ cluster.py
@@ -1,3 +1,4 @@
+import pickle
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -9,27 +10,28 @@ dtypes = {"Car": "str", "MPG": "float", "Cylinders": "int", "Displacement": "flo
 df = pd.DataFrame(pd.read_csv("./cars.csv", header=0, dtype=dtypes))
 
 # Choisir les colonnes qui serviront pour le clustering
-colonnes = ["Acceleration", "Horsepower"]
+colonnes = ["Weight", "MPG"]
 data = df.loc[:, [colonnes[0], colonnes[1]]]
 
 def kmeans(np_array, k):
     diff = 1
     cluster = np.zeros(np_array.shape[0])
+    # Génération des centroids par défaut
     centroids = data.sample(n=k).values
     while diff:
-    # for each observation
         for i, row in enumerate(np_array):
             mn_dist = float('inf')
-            # dist of the point from all centroids
+            # Calcul de la distance du point avec tout les centroids
             for idx, centroid in enumerate(centroids):
                 d = np.sqrt((centroid[0] - row[0])**2 + (centroid[1] - row[1])**2)
-                # store closest centroid
+                # Sauvegarde de la distance la plus courte
                 if mn_dist > d:
                     mn_dist = d
                     cluster[i] = idx
+        # Mise à jour des centroids avec les cluster générés
         new_centroids = pd.DataFrame(np_array).groupby(by=cluster).mean().values
-    # if centroids are same then leave
-        if np.count_nonzero(centroids-new_centroids) == 0:
+        # Si les nouveaux centroids n'ont pas changés on quitte la boucle
+        if np.count_nonzero(centroids - new_centroids) == 0:
             diff = 0
         else:
             centroids = new_centroids
@@ -42,11 +44,13 @@ if __name__ == '__main__':
     plt.xlabel(colonnes[0])
     plt.ylabel(colonnes[1])
     plt.show()
-    
 
     #Choix du nombre de cluster grace à number_of_cluster.py
-    k = 5
+    k = 4
     centroids, cluster = kmeans(np_array, k)
+    # Sauvegarde des clusters et centroids dans le dossier data
+    with open("data/" + colonnes[0] + "_" + colonnes[1] + "_clusters", "wb") as f:
+        pickle.dump((cluster, centroids), f)
     sns.scatterplot(x=np_array[:, 0], y=np_array[:, 1], hue=cluster)
     sns.scatterplot(x=centroids[:, 0], y=centroids[:, 1], s=100, color='y')
     plt.xlabel(colonnes[0])
--- number_of_clusters.py
+++ number_of_clusters.py
@@ -4,6 +4,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+# Permet de calculer le cout WCSS (Within Cluster Sum of Square)
 def calculate_cost(np_array, centroids, cluster):
     sum = 0
     for i, val in enumerate(np_array):
@@ -12,17 +14,25 @@ def calculate_cost(np_array, centroids, cluster):
 
 def find_optimal_number_of_cluster(np_array):
     cost_list = []
+    best_k = 0
+    # Calcul du cout WCSS pour un interval de 1 à 10 cluster (Methode Elbow)
     for k in range(1, 10):
         centroids, cluster = kmeans(np_array, k)
-        # WCSS (Within cluster sum of square)
         cost = calculate_cost(np_array, centroids, cluster)
         cost_list.append(cost)
-    return cost_list
+    for i in range(1, len(cost_list[1:])):
+        # Si la diminution en cout WCSS entre un k et un k + 1 est inférieur à 20 % on considère que K est le nombre
+        # optimal de cluster (peu fiable mais automatique)
+        if (cost_list[i] / cost_list[i - 1]) >= 0.8:
+            best_k = i
+            break
+    return best_k, cost_list
 
 if __name__ == '__main__':
     np_array = data.values
-    cost_list = find_optimal_number_of_cluster(np_array)
+    best_k, cost_list = find_optimal_number_of_cluster(np_array)
     sns.lineplot(x=range(1, 10), y=cost_list, marker='o')
     plt.xlabel('k')
     plt.ylabel('WCSS')
+    plt.title("best number of cluster = " + str(best_k))
     plt.show()
